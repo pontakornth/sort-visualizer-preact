c4b1c5afbaeb610f238aea8e9aec4dd6cf322b40
pontakornth <pontakorn61@gmail.com>
2021-05-14 20:16:33 +0700

    fix: Incorrect result

--- src/algorithms/mergeSort.ts
+++ src/algorithms/mergeSort.ts
@@ -2,55 +2,29 @@ import { Algorithm } from "../useVisualizer"
 function merge(arr: number[], start: number, mid: number, end: number, pushQueue: Parameters<Algorithm>[1]) {
     // [start, mid] First Section to merge
     // [mid + 1, stop] Second section to merge
-    const tempArray: number[] = []
-    let i = start
-    let j = mid + 1
-    let k = 0 // Index of temporary array
+    let tempArray: number[] = [...Array(end - start + 1).keys()]
+    const left = arr.slice(start, mid + 1)
+    const right = arr.slice(mid + 1, end  + 1)
 
-    while (i <= mid && j <= end) {
-        if (arr[i] <= arr[j]) {
-            tempArray[k] = arr[i]
-            pushQueue({
-                arr: [...arr],
-                colorIndexes: [i]
-            })
-            k++
-            i++
+    let tempIndex = 0
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            tempArray.push(left[0])
+            left.shift()
         } else {
-            tempArray[k] = arr[j]
-            pushQueue({
-                arr: [...arr],
-                colorIndexes: [j]
-            })
-            k++
-            j++
+            tempArray.push(right[0])
+            right.shift()
         }
     }
 
-    // Left over
-    while (i <= mid) {
-        tempArray[k] = arr[i]
-        i++
-            pushQueue({
-                arr: [...arr],
-                colorIndexes: [i]
-            })
-    }
+    tempArray = [...tempArray, ...left, ...right]
 
-    while (j <= end) {
-        tempArray[k] = arr[j]
-        j++
+    for (let l = start; l <= end; l++) {
+        // console.log([...arr], l - start)
+        arr[l] = tempArray[l - start]
         pushQueue({
             arr: [...arr],
-            colorIndexes: [j]
-        })
-    }
-
-    for (let i = start; i <= end; i++) {
-        arr[i] = tempArray[i - start]
-        pushQueue({
-            arr: [...arr],
-            colorIndexes: [i]
+            colorIndexes: [l]
         })
     }
 }
@@ -60,11 +34,18 @@ const mergeSort: Algorithm = (arr, pushQueue) => {
         if (start < end) {
             let mid = Math.floor((start + end) / 2)
             mergeSortInner(arr, start, mid)
+            console.log(`Sort from ${start} to ${mid}`)
             mergeSortInner(arr, mid + 1, end)
+            console.log(`Sort from ${mid + 1} to ${end}`)
             merge(arr, start, mid, end, pushQueue)
         }
     }
     mergeSortInner(arr, 0, arr.length - 1)
+    pushQueue({
+        arr: [...arr],
+        colorIndexes: []
+    })
+    console.log(arr)
 }
 
 export default mergeSort
