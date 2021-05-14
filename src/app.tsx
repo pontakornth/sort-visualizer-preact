1e2a1f8225fa5307f114732b04e81f286db39399
pontakornth <pontakorn61@gmail.com>
2021-05-14 14:36:19 +0700

    feat: Implement drawing

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,27 @@
+import { useRef } from 'preact/hooks'
 import { Logo } from './logo'
+import useVisualizer from './useVisualizer';
+import {
+  bubbleSort,
+  insertionSort,
+  selectionSort
+} from "./algorithms"
 
 export function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const { drawAlgorithm } = useVisualizer(canvasRef)
+  const draw = (algorithm: Parameters<typeof drawAlgorithm>[0]) => {
+    return () => drawAlgorithm(algorithm)
+  }
   return (
     <>
     <h1>Sort Visualizer</h1>
     <main>
-      <canvas width="480" height="480"></canvas>
+      <canvas width="480" height="480" className="visualizer" ref={canvasRef}></canvas>
       <div class="stack">
-          <button>Bubble Sort</button>
-          <button>Insertion Sort</button>
-          <button>Selection Sort</button>
+          <button onClick={draw(bubbleSort)}>Bubble Sort</button>
+          <button onClick={draw(insertionSort)}>Insertion Sort</button>
+          <button onClick={draw(selectionSort)}>Selection Sort</button>
       </div>
     </main>
     </>
